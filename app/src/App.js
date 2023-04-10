ab6176e8d54f3f5b0042816b4e39e895245bbcbf
GuskovAlexey <guskovpln@gmail.com>
2023-04-10 17:44:48 +0200

    wip

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HELLO DOSS-11 2023!
+          HELLO DOSS-11 CHANGE!
         </p>
         <a
           className="App-link"
